434bdd7ac28a742942dbc1d5559fa96220570d69
James O'Neill <james@jamesoneill.eu>
2017-09-23 10:08:18 +0100

    Initial commit

---
+++ resources/views/holding.blade.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Place Pupper | The World's H*ckin' Greatest Placeholder Images</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #fff;
+                font-family: 'Ubuntu', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .overflow-hidden {
+                overflow: hidden;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                overflow: hidden;
+                text-align: center;
+            }
+
+            .title {
+                box-sizing: border-box;
+                font-size: 84px;
+                max-width: 800px;
+                background-color: rgba(0, 0, 0, 0.5);
+                padding: 25px;
+                width: 100%;
+            }
+
+            @media screen and (max-width: 650px) {
+                .title {
+                    font-size: 65px;
+                }
+            }
+
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+            .background-pupper {
+                background-image: url("/img/dog-2540086_1920.jpg");
+                background-size: cover;
+            }
+
+            @media screen and (min-width: 1450px) {
+                .background-pupper {
+                    background-image: url("/img/chihuahua-624924_1920.jpg");
+                }
+            }
+
+            h1 small {
+                margin-top: 20px;
+                font-size: 20px;
+                display: block;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height background-pupper">
+
+            <div class="content">
+                <h1 class="title m-b-md">
+                    The World's H*ckin' Greatest Placeholder Images
+                    <small>Coming Soon</small>
+                </h1>
+            </div>
+        </div>
+    </body>
+</html>
